fc8f4c5e427c183df56b7589cb79faf695e761c2
emadjabbour <dadojab@gmail.com>
2017-12-17 18:32:50 +0200

    Add files via upload

---
+++ emad_sd_read_write.ino
@@ -0,0 +1,31 @@
+#include<SD.h>
+#include<SPI.h>
+
+File Emadtxt;
+
+void setup() {
+Serial.begin(9600);
+if(!SD.begin(4)){
+  Serial.println("SD card Faild");
+  return;
+}
+Serial.println("SD card OK");
+Emadtxt = SD.open("emad.txt",FILE_WRITE);
+if(Emadtxt){
+  Serial.println("writing on sd card");
+  Emadtxt.println("test test emad emad ok");
+  Emadtxt.close();
+}
+Emadtxt = SD.open("emad.txt");
+if(Emadtxt){
+while(Emadtxt.available()){
+  Serial.write(Emadtxt.read());
+}
+
+ Emadtxt.close();
+}
+}
+
+void loop() {
+
+}
